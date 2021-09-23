eb1711a6950ab1028b34b34fa7826ff342feb038
Whatislove00 <vlad.ik.2001@bk.ru>
2021-09-23 23:39:04 +0300

    Задание:
    3. Пользователь вводит число с плавающей точкой. Считать число и вывести отдельно целую и дробную часть. Предусмотреть возможность наличия ведущих нулей у дробной части.
    Бонус: предусмотреть обработку ограничений ввода (например, если число считывается в виде строки, то завершать работу программы при превышении заданной длины).
    Пример ввода: 10.05
    Пример вывода: 10 | 05

--- Dz/Dz/1.c
+++ Dz/Dz/1.c
@@ -1,1 +1,17 @@
-12312313213312
+#include <stdio.h>
+
+int main()
+{
+    char input[256], i;
+    fgets(input, 255, stdin);
+    for (i = 0; input[i] != '.'; ++i)
+    {
+        printf("%c", input[i]);
+    }
+    ++i;
+    printf(" | ");
+    for (; input[i] != '\0'; ++i)
+    {
+        printf("%c", input[i]);
+    }
+}
